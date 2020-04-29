946b1eb3dbe6a99cb895849c052b100e272d3595
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 17:19:15 -0300

    103

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 
   
    const [dataEmpresa] = useFetch(
-      "https://vhali.com.ar/api/pedidos/local/" + pathArray[1]
+      "https://vhali.com.ar/api/pedidos/local/de-niro-1" //+ pathArray[1]
    );
 
     const [data] = useFetch(
-      "https://vhali.com.ar/api/pedidos/menu/" + pathArray[1]
+      "https://vhali.com.ar/api/pedidos/menu/de-niro-1" //+ pathArray[1]
     );
 	
 	
