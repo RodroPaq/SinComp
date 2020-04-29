b609bdaa96f869a50ecd833b4a85136c08bac90b
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 16:21:25 -0300

    25

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
+	
+	
+	var pathArray = window.location.pathname.split('/');
+
+  
+   const [dataEmpresa] = useFetch(
+      "http://vhali.com.ar/api/pedidos/local/de-niro-1" //+ pathArray[1]
+   );
+
+    const [data] = useFetch(
+      "http://vhali.com.ar/api/pedidos/menu/de-niro-1" //+ pathArray[1]
+    );
+	
+	
   return (
     <div className="App">
       <Header />
