5d91153cbcb2d901851b53f8ec85210dac2a41c9
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 20:01:37 -0300

    110

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import Productos from './Productos'
 
-const Menu = ({menu}) => {    
+const Header = ({menu,empresa}) => {   
     return (  
-        <div className="row mt-2 mb-5 mt-5">
-            <div className="tab-content col-12" id="nav-tabContent">           
-                {menu.map((i,k) => {
-                   return(<Productos menu={i.Subcategorias} categoria={i.Nombre} key={k} dk={k}/>)
-                })}                                  
-            </div>
+        <div className="nav-scroller fixed-top shadow-lg bg-4B4453">
+            <nav className="nav nav-underline">
+            {empresa.Imagen !== null ?
+            <img src={"Imagenes/" + empresa.Imagen} width="5%" className="d-inline-block align-top img-responsive ml-3 mt-1" alt="" />
+            :
+            <span className="nav-link active text-info" >{empresa.Nombre}</span>            
+            }
+            {menu.map((i,k) => {
+               let titulo = i.Nombre.replace(/ /g, "-");
+               let activeClass = (k === 0 ? "nav-link text-white active" : "nav-link text-white" );
+               return (<a className={activeClass} href={"#" + titulo} id={titulo + "-tab"} key={k}  data-toggle="tab" role="tab" aria-controls={titulo} aria-selected="true">{i.Nombre}</a>)               
+            })}
+             
+            </nav>
         </div>
     );
 }
  
-export default Menu;
+export default Header;
