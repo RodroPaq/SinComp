e5c1583f69e6167cf91a5de03663e18c3d65797e
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 17:15:54 -0300

    102

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,10 +5,11 @@ const Menu = ({menu}) => {
     return (  
         <div className="row mt-2 mb-5 mt-5">
             <div className="tab-content col-12" id="nav-tabContent"> 
-			<p>{i.Nombre}</p>
-                {/* {menu.map((i,k) => {
-                   return(<Productos menu={i.Subcategorias} categoria={i.Nombre} key={k} dk={k}/>)
-                })}                                  */}
+			
+                 {menu.map((i,k) => {
+					 return(<p>{i.Nombre}</p>)
+                   {/*return(<Productos menu={i.Subcategorias} categoria={i.Nombre} key={k} dk={k}/>)*/}
+                })}                                  
             </div>
         </div>
     );
