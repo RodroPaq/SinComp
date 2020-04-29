8afc31e5ea7617811ab14f667590427ff7ca0cd8
RodroPaq <58706760+RodroPaq@users.noreply.github.com>
2020-04-29 15:29:16 -0300

    10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
 import './App.css';
+//import Header from './components/Header';
+import Menu from './components/Menu';
+import { useFetch } from './components/HookFetch';
 
 function App() {
+
+   var pathArray = window.location.pathname.split('/');
+
+  
+  const [dataEmpresa] = useFetch(
+     "http://vhali.com.ar/api/pedidos/local/de-niro-1" //+ pathArray[1]
+  );
+
+   const [data] = useFetch(
+     "http://vhali.com.ar/api/pedidos/menu/de-niro-1" //+ pathArray[1]
+   );
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Fragment>
+      {/*<Header menu={data} empresa={dataEmpresa}/>*/}
+        <Menu  />{/*menu={data} /> */}
+     </Fragment>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,10 +4,11 @@ import Productos from './Productos'
 const Menu = ({menu}) => {    
     return (  
         <div className="row mt-2 mb-5 mt-5">
-            <div className="tab-content col-12" id="nav-tabContent">                
-                {menu.map((i,k) => {
+            <div className="tab-content col-12" id="nav-tabContent"> 
+            <p>|||||||||||||||||</p>               
+                {/* {menu.map((i,k) => {
                    return(<Productos menu={i.Subcategorias} categoria={i.Nombre} key={k} dk={k}/>)
-                })}                                 
+                })}                                  */}
             </div>
         </div>
     );
